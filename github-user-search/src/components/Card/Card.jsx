2157557ba7f2471a72c4b960e1f99a41146ed277
gfunk77 <faraci77@gmail.com>
2024-05-05 15:40:08 -0400

    mobile card header and para

--- github-user-search/src/components/Card/Card.jsx
+++ github-user-search/src/components/Card/Card.jsx
@@ -1,6 +1,25 @@
 import './card.css';
 
 function Card({ theme }) {
-  return <div className={`card ${theme}`}>Card</div>;
+  return (
+    <article className={`card ${theme}`}>
+      <div className="img-container">
+        <img className="img" src="https://picsum.photos/70" alt="" />
+      </div>
+      <header className="card-header">
+        <div className="info">
+          <h2 className="username">The Octocat</h2>
+          <p className="handle">@octocat</p>
+        </div>
+        <p className="joined">Joined 25 Jan 2011</p>
+      </header>
+      <div className="card-body">
+        <p className="card-desc">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat
+          mattis eros.
+        </p>
+      </div>
+    </article>
+  );
 }
 export default Card;
