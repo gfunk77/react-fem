9ad203bc102b31f9f11db79199a8693361ee4ccb
gfunk77 <faraci77@gmail.com>
2024-05-05 20:46:45 -0400

    stats

--- github-user-search/src/components/Card/Card.jsx
+++ github-user-search/src/components/Card/Card.jsx
@@ -24,6 +24,14 @@ function Card({ theme, user }) {
         <p className="card-desc">
           {user.bio || 'This is a mysterious individual without a biography. Use your imagination!'}
         </p>
+        <div className="stats">
+          <p>Repos</p>
+          <p>Followers</p>
+          <p>Following</p>
+          <p>{user.public_repos}</p>
+          <p>{user.followers}</p>
+          <p>{user.following}</p>
+        </div>
       </div>
     </article>
   );
