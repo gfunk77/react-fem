c0e455dd3797105b3ffa7450596efe0729f9b2a9
gfunk77 <faraci77@gmail.com>
2025-01-07 13:48:56 -0500

    dropdown with temp data

--- rest-countries/src/App.jsx
+++ rest-countries/src/App.jsx
@@ -1,3 +1,4 @@
+import Dropdown from './components/Dropdown';
 import Navbar, { Logo, ThemeToggle } from './components/Navbar';
 import Search from './components/Search';
 
@@ -11,6 +12,8 @@ function App() {
       {/* <div className="mx-auto mt-6 px-4 md:container:px-0 "> */}
       <div className="px-4 md:px-20 mt-6 transition-all">
         <Search />
+        <br />
+        <Dropdown />
       </div>
     </>
   );
---
+++ rest-countries/src/components/Dropdown.jsx
@@ -0,0 +1,47 @@
+import { IoChevronDown } from 'react-icons/io5';
+import { useState, useRef } from 'react';
+
+function Dropdown() {
+  const [selected, setSelected] = useState('Filter by Region');
+  const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  const dropdownMenuRef = useRef(null);
+
+  function toggleMenu() {
+    if (dropdownMenuRef.current) {
+      dropdownMenuRef.current.classList.toggle('hidden');
+    }
+  }
+
+  function handleSelect(region) {
+    setSelected(region);
+    if (dropdownMenuRef.current) {
+      dropdownMenuRef.current.classList.add('hidden');
+    }
+  }
+
+  return (
+    <div className="dropdown">
+      <div
+        tabIndex={0}
+        role="button"
+        className="input flex items-center justify-between text-xs leading-5 font-normal border-none rounded shadow w-52 md:text-sm md:h-14"
+        onClick={toggleMenu}>
+        {selected}
+        <IoChevronDown />
+      </div>
+      <ul
+        ref={dropdownMenuRef}
+        tabIndex={0}
+        className="dropdown-content menu bg-base-100 rounded
+         z-[1] w-full max-w-52 p-2 shadow hidden">
+        {regions.map((region) => (
+          <li key={region}>
+            <a onClick={() => handleSelect(region)}>{region}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Dropdown;
