ea6e19cd39e93d5f73237f93d236e99b39872ae4
gfunk77 <faraci77@gmail.com>
2025-01-07 11:20:25 -0500

    adjust border and placeholder text size

--- rest-countries/src/components/Search.jsx
+++ rest-countries/src/components/Search.jsx
@@ -4,8 +4,8 @@ function Search() {
   return (
     <label
       style={{ maxWidth: '480px' }}
-      className="input flex items-center gap-6 text-xs leading-5 font-normal border-none shadow-md pl-8">
-      <IoSearch style={{ color: 'C4C4C4' }} className="text-base font-semibold" />
+      className="input flex items-center gap-6 text-xs leading-5 font-normal border-none rounded shadow-md pl-8 md:text-sm md:h-14">
+      <IoSearch style={{ color: 'C4C4C4' }} className="text-base font-semibold md:text-lg" />
       <input type="search" className="grow" placeholder="Search for a country..." />
     </label>
   );
