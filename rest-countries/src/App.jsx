36615f2cc39c01040d44061cc24eac8284892df3
gfunk77 <faraci77@gmail.com>
2025-01-13 14:30:19 -0500

    cache results for dropdown

--- rest-countries/src/App.jsx
+++ rest-countries/src/App.jsx
@@ -2,6 +2,16 @@ import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import BaseLayout from './pages/BaseLayout';
 import Landing, { loader as landingLoader } from './pages/Landing';
 import Error from './components/Error';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -28,6 +38,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
 }
 export default App;
--- rest-countries/src/pages/Landing.jsx
+++ rest-countries/src/pages/Landing.jsx
@@ -4,6 +4,7 @@ import CountriesList from '../components/CountriesList';
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
 
 const baseUrl = 'https://restcountries.com/v3.1';
 const codes = ['deu', 'usa', 'bra', 'isl', 'afg', 'ala', 'alb', 'dza'];
@@ -31,17 +32,27 @@ export const loader = async ({ request }) => {
 function Landing() {
   const { countries, q } = useLoaderData();
   const [filteredCountries, setFilteredCountries] = useState(countries);
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     setFilteredCountries(countries);
   }, [countries]);
 
+  const getRegionCountries = async (region) => {
+    const response = await axios.get(`${baseUrl}/region/${region}`);
+    return response.data;
+  };
+
   const handleSelectedRegion = async (region) => {
     if (region === 'Filter by Region') {
       setFilteredCountries(countries);
     } else {
-      const response = await axios.get(`${baseUrl}/region/${region}`);
-      setFilteredCountries(response.data);
+      const data = await queryClient.fetchQuery({
+        queryKey: ['regionCountries', region],
+        queryFn: () => getRegionCountries(region),
+      });
+
+      setFilteredCountries(data);
     }
   };
 
