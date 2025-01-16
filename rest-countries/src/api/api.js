5183997bfa2fb56bd73277e609017f2e00d5fae2
gfunk77 <faraci77@gmail.com>
2025-01-15 20:44:20 -0500

    refactor api calls and loader

---
+++ rest-countries/src/api/api.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const baseUrl = 'https://restcountries.com/v3.1';
+const codes = ['deu', 'usa', 'bra', 'isl', 'afg', 'ala', 'alb', 'dza'];
+const codesQuery = `/alpha?codes=${codes.join(',')}`;
+
+export const getInitialData = async () => {
+  try {
+    const response = await axios.get(baseUrl + codesQuery);
+
+    const filteredCountries = codes.map((code) =>
+      response.data.find((country) => country.cca3.toLowerCase() === code)
+    );
+
+    return filteredCountries;
+    // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    throw new Error('Failed to fetch data');
+  }
+};
+
+export const getSearch = async (searchTerm) => {
+  try {
+    const response = await axios.get(`${baseUrl}/name/${searchTerm}`);
+
+    return response.data;
+    // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    throw new Error(`Country with name ${searchTerm} not found`);
+  }
+};
+
+export const getRegion = async (region) => {
+  try {
+    const response = await axios.get(`${baseUrl}/region/${region}`);
+
+    return response.data;
+    // eslint-disable-next-line no-unused-vars
+  } catch (error) {
+    throw new Error(`Region ${region} not found`);
+  }
+};
--- rest-countries/src/pages/Landing.jsx
+++ rest-countries/src/pages/Landing.jsx
@@ -1,50 +1,9 @@
 import Search from '../components/Search';
 import Dropdown from '../components/Dropdown';
 import CountriesList from '../components/CountriesList';
-import axios from 'axios';
-import { useLoaderData, useSearchParams } from 'react-router-dom';
+import { getInitialData, getRegion, getSearch } from '../api/api';
+import { useLoaderData } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-
-const baseUrl = 'https://restcountries.com/v3.1';
-const codes = ['deu', 'usa', 'bra', 'isl', 'afg', 'ala', 'alb', 'dza'];
-const codesQuery = `/alpha?codes=${codes.join(',')}`;
-
-const getInitialData = async () => {
-  try {
-    const response = await axios.get(baseUrl + codesQuery);
-
-    const filteredCountries = codes.map((code) =>
-      response.data.find((country) => country.cca3.toLowerCase() === code)
-    );
-
-    return filteredCountries;
-    // eslint-disable-next-line no-unused-vars
-  } catch (error) {
-    throw new Error('Failed to fetch data');
-  }
-};
-
-const getSearch = async (searchTerm) => {
-  try {
-    const response = await axios.get(`${baseUrl}/name/${searchTerm}`);
-
-    return response.data;
-    // eslint-disable-next-line no-unused-vars
-  } catch (error) {
-    throw new Error(`Country with name ${searchTerm} not found`);
-  }
-};
-
-const getRegion = async (region) => {
-  try {
-    const response = await axios.get(`${baseUrl}/region/${region}`);
-
-    return response.data;
-    // eslint-disable-next-line no-unused-vars
-  } catch (error) {
-    throw new Error(`Region ${region} not found`);
-  }
-};
 
 export const loader =
   (queryClient) =>
@@ -53,31 +12,18 @@ export const loader =
     const search = url.searchParams.get('search');
     const region = url.searchParams.get('region');
 
-    let data;
+    const queryKey = search
+      ? ['searchCountries', search]
+      : region
+      ? ['regionCountries', region]
+      : ['initialData'];
+    const queryFn = search
+      ? () => getSearch(search)
+      : region
+      ? () => getRegion(region)
+      : () => getInitialData();
 
-    if (search) {
-      data = await queryClient.ensureQueryData({
-        queryKey: ['searchCountries', search],
-        queryFn: () => getSearch(search),
-      });
-      if (!data) {
-        throw new Response({
-          status: 404,
-          statusText: 'Not Found',
-          message: `Country with name ${search} not found`,
-        });
-      }
-    } else if (region) {
-      data = await queryClient.ensureQueryData({
-        queryKey: ['regionCountries', region],
-        queryFn: () => getRegion(region),
-      });
-    } else {
-      data = await queryClient.ensureQueryData({
-        queryKey: ['initialData'],
-        queryFn: () => getInitialData(),
-      });
-    }
+    const data = await queryClient.ensureQueryData({ queryKey, queryFn });
 
     return { countries: data };
   };
@@ -85,8 +31,6 @@ export const loader =
 function Landing() {
   const { countries } = useLoaderData();
   const [filteredCountries, setFilteredCountries] = useState(countries);
-  // eslint-disable-next-line no-unused-vars
-  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     setFilteredCountries(countries);
